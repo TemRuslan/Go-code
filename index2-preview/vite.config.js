b27b6497a35b5a2249df3467b81a6fb16a1ac719
Ruslan Temirkhanov <tem.ruslan97@gmail.com>
2026-02-07 21:30:31 +0300

    Настроить запуск index2 локально

--- index2-preview/vite.config.js
+++ index2-preview/vite.config.js
@@ -3,5 +3,8 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  // GitHub Pages project site: https://<user>.github.io/<repo>/
+  // We publish the built app under /go-code/index2/
+  base: "/go-code/index2/",
   plugins: [react()],
 });
